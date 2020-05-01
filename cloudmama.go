4002403f63509592e6813b89606ee86b026329de
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2020-05-02 05:30:02 +0900

    complete prototype

--- cloudmama.go
+++ cloudmama.go
@@ -3,6 +3,7 @@ package main
 import (
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	"context"
+	"fmt"
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 	"log"
 	"time"
@@ -26,7 +27,7 @@ func Speech(text string) []byte{
 		// Build the voice request, select the language code ("en-US") and the SSML
 		// voice gender ("neutral").
 		Voice: &texttospeechpb.VoiceSelectionParams{
-			LanguageCode: "ja-JP-Wavenet-A",
+			LanguageCode: "ja-JP-Standard-A",
 			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
 		},
 		// Select the type of audio file you want returned.
@@ -42,9 +43,38 @@ func Speech(text string) []byte{
 
 	return resp.AudioContent
 }
+func Now(timezone string) time.Time{
+	now:=time.Now()
+	if timezone==""{
+		timezone="Asia/Tokyo"
+	}
+	if l,e:=time.LoadLocation(timezone);e==nil{
+		now=now.In(l)
+	}
+	return now
+}
 func main() {
+	Handle("/speech", func(w Response, r Request) {
+		now:=Now(r.FormValue("tz"))
+		report :=fmt.Sprintf("%d時%d分、今日は残り%d%%です。",now.Hour(),now.Minute(),100-100*(now.Hour()*60+now.Minute())/1440)
+		advice :="手帳を持っていますか？ベッドを畳んでいますか？スマートフォンを遠ざけていますか？"
+		if 20==now.Hour(){
+			advice ="明日に備えてやるべきことは「連絡、充電、着替え、風呂と洗濯、掃除、日記」です。"
+		}else if now.Hour()==3{
+			advice =fmt.Sprintf("ベッドを畳み、日記を読み、%d日の計画を立てましょう。",now.Day())
+			advice +="身支度の手順は「歯磨き、洗顔、薬を飲む、肌、毛を剃る」です。"
+			advice +="持ち物は「財布、鍵、携帯、筆箱、手帳、眼鏡」です。"
+		}else if 21<=now.Hour() || now.Hour()<3{
+			advice ="寝ましょう。睡眠に最適な時間帯は午後10時から午前2時です。"
+		}
+		w.Write(Speech(report + advice))
+	})
+	Handle("/text", func(w Response, r Request) {
+		w.Write(Speech(r.FormValue("q")))
+	})
 	Handle("/", func(w Response, r Request) {
-		w.Write(Speech(time.Now().Format("2006年01月02日 15時04分05秒")))
+		now:=Now(r.FormValue("tz"))
+		w.Write(Speech(fmt.Sprintf("%d月%d日は残り%d%%です。",now.Month(),now.Day(),100-100*(now.Hour()*60+now.Minute())/1440)))
 	})
 	Credentialize("cloudmama.user.json")
 	Listen()
