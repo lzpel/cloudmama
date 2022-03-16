5a0ddd1e071c8895701e44972151b92d1cf27a2e
lzpel <18492524+lzpel@users.noreply.github.com>
2022-03-16 20:34:04 +0900

    update

--- esp32/setup.sh
+++ esp32/setup.sh
@@ -3,8 +3,8 @@ cd `dirname $0`
 set -eux
 mkdir -p components
 cd components
-curl -L https://github.com/espressif/arduino-esp32/archive/refs/tags/2.0.2.tar.gz | tar zx -C ./ && mv arduino* arduino
-curl -L https://github.com/espressif/esp32-camera/archive/refs/tags/v2.0.0.tar.gz | tar zx -C ./ && mv esp32-camera* esp32-camera
+rm -rf arduino* && curl -L https://github.com/espressif/arduino-esp32/archive/refs/tags/1.0.6.tar.gz | tar zx -C ./ && mv arduino* arduino
+rm -rf esp32-camera && curl -L https://github.com/espressif/esp32-camera/archive/refs/tags/v2.0.0.tar.gz | tar zx -C ./ && mv esp32-camera* esp32-camera
 # https://esp32.com/viewtopic.php?t=22444
 # You are looking for the ESP32 Camera Driver. Excellent! Thanks for the help! So I guess this is not part of ESP-IDF but still under Espressif.
 
