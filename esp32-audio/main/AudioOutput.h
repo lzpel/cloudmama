c1b09fc8c4af8248453abc0d8422fdb5845621c7
lzpel <18492524+lzpel@users.noreply.github.com>
2023-01-09 20:30:29 +0900

    audio

---
+++ esp32-audio/main/AudioOutput.h
@@ -0,0 +1,18 @@
+//
+// Created by smith on 2023/01/09.
+//
+
+#ifndef CLOUDMAMA_AUDIO_H
+#define CLOUDMAMA_AUDIO_H
+
+#include "driver/i2s_std.h"
+
+class AudioOutput {
+    i2s_chan_handle_t tx_handle;
+    void start();
+    void play(void(*callback)(int16_t *p, signed size));
+    void end();
+};
+
+
+#endif //CLOUDMAMA_AUDIO_H
